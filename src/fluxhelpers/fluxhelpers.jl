96e401c1984a1b38595fb729e9f4be598a1039e1
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2021-12-08 13:49:27 -0700

    generic jacobian for funcitons in first layer

--- src/fluxhelpers/fluxhelpers.jl
+++ src/fluxhelpers/fluxhelpers.jl
@@ -12,14 +12,13 @@ using DiffEqFlux: FastChain, FastDense
 
 function ltril(v::AbstractVector)
     N = length(v)
-    M = floor(Int, (-1 + sqrt(1 + 4*2*N))/2)
-    P = cumsum(0:M-1)
-    @inbounds columns = map(i->v[P.+i], 1:M)
-    Q = reduce(hcat, columns)
-    L = LowerTriangular(Q)
+    M = isqrt(N)
+    LowerTriangular(reshape(v,M,M))
 end
-
-function posdef(v::AbstractVector,::Any=nothing)
+function ltril(V::AbstractMatrix)
+    cat(map(ltril, eachcol(V))..., dims=(1,2))
+end
+function posdef(v::AbstractVecOrMat,::Any=nothing)
     L = ltril(v)
     L*L'
 end
@@ -28,6 +27,9 @@ function skewsym(v::AbstractVector,::Any=nothing)
     N = floor(Int, sqrt(length(v)))
     A = reshape(v,N,N)
     S = A - A'
+end
+function skewsym(V::AbstractMatrix,::Any=nothing)
+    cat(map(skewsym, eachcol(V))..., dims=(1,2))
 end
 
 const SubMatrix = SubArray{T,2} where {T}
@@ -52,20 +54,24 @@ derivative(f::typeof(tanh)) = dtanh
 derivative(f::typeof(identity)) = (x)->one(eltype(x))
 derivative(::typeof(square)) = (x,::Any=nothing)->2x
 
+jacobian(::typeof(square), x::AbstractVector, ::Any=nothing) = diagm(derivative(square)(x))
+jacobian(l::Dense, x) = hadamard(derivative(l.σ).(x), l.W)
+function jacobian(l::FastDense, x, p)
+    W, _ = param2Wb(l, p)
+    σbar = derivative(l.σ).(x)
+    return hadamard(σbar, W)
+end
+function jacobian(::F, x, ::Any=nothing) where {F<:Function}
+    error("Analytical jacobian of $F required, but not implemented.")
+end
+
 struct ActivationMode{T} end
 const NoActivation = ActivationMode{:NoActivation}()
 const DefaultActivation = ActivationMode{:Default}()
 
-function jacobian(::F, x) where {F<:Function}
-    error("Analytical jacobian of $F required, but not implemented.")
-    # ∇ = Flux.jacobian(f, x)[1]
-    # return ∇
-end
-
 # Flux ####################################################
 
 const DenseLayers = NTuple{N,Dense} where {N}
-const OddDense = Tuple{typeof(square),Vararg{Dense,N}} where {N}
 const InputDense = Tuple{<:Function,Vararg{Dense,N}} where {N}
 
 function (a::Dense)(x::AbstractVecOrMat, s::ActivationMode)
@@ -78,25 +84,17 @@ function _applychain(fs::DenseLayers, x)
     y = Flux.applychain(front(fs),x)
     last(fs)(y, NoActivation)
 end
+_applychain(fs::InputDense, x) = _applychain(tail(fs), first(fs)(x))
+_applychain(fs::Tuple{<:Function}, x) = x
+
 
 chainGrad(fs::Tuple{}, x) = LinearAlgebra.I
-function chainGrad(fs::DenseLayers, x)
-    y = _applychain(fs,x)
-    σbar = derivative(last(fs).σ).(y)
-    hadamard(σbar, last(fs).W) * chainGrad(front(fs), x)
-end
-function chainGrad(fs::OddDense, x)
-    J = chainGrad(tail(fs), first(fs)(x))
-    xbar = derivative(first(fs))(x)
-    return J .* repeat(xbar', size(J,1))
-end
-function chainGrad(fs::InputDense, x)
-    xbar = first(fs)(x)
-    J = chainGrad(tail(fs), xbar)
-    return J * jacobian(first(fs), xbar)
+function chainGrad(fs::T, x)  where {T<:Union{DenseLayers,InputDense}}
+    y = _applychain(fs,x) 
+    jacobian(last(fs), y) * chainGrad(front(fs), x)
 end
 
-function jacobian(m::Chain{T}, x) where {T<:Union{DenseLayers,OddDense,InputDense}}
+function jacobian(m::Chain{T}, x) where {T<:Union{DenseLayers,InputDense}}
     chainGrad(m.layers,x)
 end
 function jacobian(c::Chain, x) 
@@ -112,7 +110,7 @@ function jacobian(c::Chain, x)
         end
     elseif fout === typeof(skewsym)
         ∂L∂x = map(eachcol(J)) do col
-            dL = skewsym(-col)
+            dL = skewsym(col)
         end 
     else
         error("Analytical Jacobian not supported for this Chain type. Use AD.")
@@ -135,7 +133,6 @@ makeskewsym(m::Chain) = Chain(m.layers..., skewsym)
 # # DiffEqFlux ##############################################
 
 const FastDenseLayers = NTuple{N,FastDense} where {N}
-const OddFastDense = Tuple{typeof(square),Vararg{FastDense,N}} where {N}
 const InputFastDense = Tuple{<:Function,Vararg{FastDense,N}} where {N}
 
 function (a::FastDense)(x::AbstractVecOrMat, p, s::ActivationMode)
@@ -143,34 +140,24 @@ function (a::FastDense)(x::AbstractVecOrMat, p, s::ActivationMode)
     W, b = param2Wb(a, p)
     return W*x .+ b
 end
-function _applychain(fs::Tuple, x, p)
+function _applychain(fs::FastDenseLayers, x, p)
     ps = paramslice(fs,p)
     psvec = reduce(vcat,front(ps))
     y = DiffEqFlux.applychain(front(fs),x,psvec)
     last(fs)(y, last(ps), NoActivation)
 end
 _applychain(fs::Tuple{FastDense}, x, p) = last(fs)(x, p, NoActivation)
+_applychain(fs::InputFastDense, x, p) = _applychain(tail(fs), first(fs)(x), p)
+_applychain(fs::Tuple{<:Function}, x, p) = x
 
-function chainGrad(fs::FastDenseLayers, x, p)
+function chainGrad(fs::InputFastDense, x, p)
     y = _applychain(fs,x,p)
     ps = last(paramslice(fs,p))
-    W, _ = param2Wb(last(fs), ps)
-    σbar = derivative(last(fs).σ).(y)
-    return hadamard(σbar, W) * chainGrad(front(fs), x, p[1:end-length(ps)])
-end
-function chainGrad(fs::OddFastDense, x, p)
-    J = chainGrad(tail(fs), first(fs)(x), p)
-    xbar = derivative(first(fs))(x,p)
-    return J .* repeat(xbar', size(J,1))
-end
-function chainGrad(fs::InputFastDense, x, p)
-    xbar = first(fs)(x)
-    J = chainGrad(tail(fs), xbar, p)
-    return J * jacobian(first(fs), xbar)
+    return jacobian(last(fs), y, ps) * chainGrad(front(fs), x, p[1:end-length(ps)])
 end
 chainGrad(fs::Tuple{}, x, p) = LinearAlgebra.I
 
-function jacobian(m::FastChain{T}, x, p) where {T<:Union{FastDenseLayers,OddFastDense,InputFastDense}}
+function jacobian(m::FastChain{T}, x, p) where {T<:Union{FastDenseLayers,InputFastDense}}
     chainGrad(m.layers,x,p)
 end
 function jacobian(c::FastChain, x, p) 
@@ -186,7 +173,7 @@ function jacobian(c::FastChain, x, p)
         end
     elseif fout === typeof(skewsym)
         ∂L∂x = map(eachcol(J)) do col
-            dL = skewsym(-col)
+            dL = skewsym(col)
         end 
     else
         error("Analytical Jacobian not supported for this Chain type. Use AD.")
