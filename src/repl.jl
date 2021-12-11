4b534385e241ad1fcc7f77afbe0920f6ad51b9cb
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2021-12-11 12:53:45 -0700

    more notes

--- src/repl.jl
+++ src/repl.jl
@@ -30,13 +30,14 @@ Md⁻¹ = M⁻¹
 #     SOSPoly(4,1:2)
 # )
 Vd = FastChain(
-        FastDense(4, 10, elu),
+        FastDense(2, 10, elu),
         FastDense(10, 5, elu),
         FastDense(5, 1, square),
 )
 P = IDAPBCProblem(2,M⁻¹,Md⁻¹,V,Vd,G,G⊥)
 L1 = PDELossPotential(P)
 ps = paramstack(P)
+u_idapbc(x,p) = controller(P,x,p)
 
 function eom(x,u)
     I1 = 0.0455
@@ -60,21 +61,34 @@ function eom!(dx,x,u)
     dx[3] = m3*sin(q1)/I1 - u/I1
     dx[4] = u/I2
 end
-# u(x,p) = controller(P,x,p)
-u(x,p) = sum(jacobian(Vd,x,p))/length(p)
 
-sys = ParametricControlSystem{!true}(eom,u,4)
-prob = ODEProblem(sys, ps, (0.0, 3.0))
+
+Hd = FastChain(
+    FastDense(4, 10, elu),
+    FastDense(10, 5, elu),
+    FastDense(5, 1)
+)
+ps2 = DiffEqFlux.initial_params(Hd)
+u_neuralpbc(x,p) = sum(jacobian(Hd,x,p))/length(p)
+
+# sys = ParametricControlSystem{true}(eom!,u_idapbc,4)
+# prob = ODEProblem(sys, ps, (0.0, 3.0))
+sys = ParametricControlSystem{!true}(eom,u_neuralpbc,4)
+prob = ODEProblem(sys, ps2, (0.0, 3.0))
+
+
 function L3(q,ps)
     #=
     Adjoint method notes:
-    - Only ReverseDiffAdjoint() works for IDAPBC
+    - InterpolatingAdjoint() with oop dynamics works w/ IDAPBC
+    - ReverseDiffAdjoint() with in-place dynamics works w/ IDAPBC
     - ReverseDiffAdjoint() tends to be the fastest w/ least mem usage
     - For dim(x)=4, oop dynamics is faster
     =#
     sum(abs2, 
         trajectory(
-            prob, q, ps, saveat=0.1, 
+            prob, q, ps; 
+            saveat=0.1,
             sensealg=DiffEqFlux.ReverseDiffAdjoint()
         )[:,end]
     )
