ec13a7dee4389375a5b532e9f7e482a451a301a7
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2021-12-17 16:46:28 -0700

    NeuralPBCLoss

--- src/neuralpbc.jl
+++ src/neuralpbc.jl
@@ -1,4 +1,5 @@
 export NeuralPBC, unstack, controller
+export SetDistanceLoss, gradient
 
 struct NeuralPBC{N,HD} 
     Hd::HD
@@ -21,3 +22,35 @@ function controller(P::NeuralPBC{N}, x, ps) where {N}
     dot(gains, jacobian(P.Hd, x, Hdθ))
 end
 
+
+
+abstract type NeuralPBCLoss end
+
+function gradient(l::NeuralPBCLoss, prob::ODEProblem, x0, θ; dt=0.1)
+    x = trajectory(prob, x0, θ; saveat=dt, sensealg=DiffEqFlux.ReverseDiffAdjoint())
+    loss(ps) = l(x)
+    val, back = Zygote.pullback(loss, θ)
+    return first(back(1)), val
+end
+function gradient(ls::Tuple{Vararg{L}}, prob::ODEProblem, x0, θ; dt=0.1) where {L<:NeuralPBCLoss}
+    loss(ps) = begin
+        x = trajectory(prob, x0, θ; saveat=dt, sensealg=DiffEqFlux.ReverseDiffAdjoint())
+        sum(l(x) for l in ls)
+    end
+    val, back = Zygote.pullback(loss, θ)
+    return first(back(1)), val
+end
+
+
+struct SetDistanceLoss{T,F} <: NeuralPBCLoss
+    xstar::Vector{T}
+    radius::T
+    f::F
+end
+function SetDistanceLoss(f::Function,xstar,r)
+    SetDistanceLoss{eltype(xstar),typeof(f)}(xstar,r,f)
+end
+function (l::SetDistanceLoss)(x::Matrix)
+    delta = mapreduce(l.f, min, eachcol(x))
+    return delta < l.radius ? zero(eltype(x)) : delta - l.radius
+end
